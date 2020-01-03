16680527375c13bd1bcc60b3705cf9ec274a46a0
Lucas Andrade <lucascorporation@live.com>
2020-01-02 21:19:18 -0300

    go back button

---
+++ src/components/GoBack.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <span class="go-back">
+            <button @click="goBack">go back</button>
+        </span>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        goBack()
+        {
+            return this.$router.go(-1);
+        }
+    },
+}
+</script>
+
+<style scoped>
+ .go-back{
+     display:flex;
+     cursor: pointer;
+ }
+ button{
+     border:0;
+ }
+</style>
--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
     <div> 
+        <GoBack/>
         <section class="destination">
             <h1>{{destination.name}}</h1>
               <div class="destination-details">
@@ -31,7 +32,11 @@
 
 <script>
 import store from "@/store.js";
+import GoBack  from "@/components/GoBack";
 export default {
+    components:{
+        GoBack,
+    },
     data() {
         return {
             // slug:this.$route.params.slug,
